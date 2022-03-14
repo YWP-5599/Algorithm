a432ef69b116dda23a03eba1f91ddcbfc97340dc
YWP-5599 <70199409+YWP-5599@users.noreply.github.com>
2022-03-14 15:30:50 +0800

    Update sortTest.java

--- src/com/scu/algorithm/sortTest.java
+++ src/com/scu/algorithm/sortTest.java
@@ -2,6 +2,8 @@ package com.scu.algorithm;
 
 public class sortTest {
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
+        int[] arr = new int[]{1,9,5,6,3,2,77};
+        sort_algorithm.sort(arr);
+        System.out.println(aRR);
+        }
 }
